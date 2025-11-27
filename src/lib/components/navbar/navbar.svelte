ca69d4716609c748bf13da5acf37cb002baa14c4
TheChirag356 <arorachirag12005@gmail.com>
2025-11-27 17:50:21 +0530

    feat: navbar component

---
+++ src/lib/components/navbar/navbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	let currentPage = $state(page.url.pathname);
+
+	$effect(() => {
+		currentPage = page.url.pathname;
+	});
+
+	import { Separator } from '$lib/components/separator';
+	import { Container } from '$lib/components/container';
+	import { cn } from '$lib/utils';
+
+	const links = [
+		{ href: '/', label: 'Portfolio' },
+		{ href: '/blog', label: 'Blog' },
+		{ href: '/components', label: 'Components' }
+	];
+</script>
+
+<header class="sticky inset-x-0 top-0 left-0 z-50 h-12 bg-background font-mono text-sm">
+	<Separator />
+	<Container>
+		<nav class="mx-auto flex h-24 max-w-5xl items-center justify-between px-4">
+			<div></div>
+			<div class="hidden h-full items-center justify-end gap-4 sm:flex">
+				{#each links as { href, label }}
+					<a
+						{href}
+						aria-current={currentPage === href}
+						class={cn(
+							'',
+							currentPage.replace('/#', '/') === href
+								? 'font-medium text-foreground'
+								: 'text-muted-foreground no-underline'
+						)}>{label}</a
+					>
+				{/each}
+			</div>
+		</nav>
+	</Container>
+	<Separator />
+</header>
