8d36dd06dbb11c46bc4c7c47d8f589ec6801ba62
TheChirag356 <arorachirag12005@gmail.com>
2025-11-27 17:49:11 +0530

    feat: footer component

---
+++ src/lib/components/footer/footer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Container } from '$lib/components/container';
+	import { Separator } from '$lib/components/separator';
+	import { cn } from '$lib/utils';
+
+	type FooterProps = {
+		class?: string;
+	};
+
+	let { class: className }: FooterProps = $props();
+</script>
+
+<Separator />
+<Container class={cn('h-20 font-mono', className)}>
+	<footer class="flex flex-col items-center justify-center text-center text-sm text-neutral-400">
+		<p>
+			Inspired by{' '}
+			<a target="blank" href={'https://x.com/iamncdai'}>@iamncdai</a>
+		</p>
+		<p>
+			Built by{' '}
+			<a target="blank" href={'https://x.com/chiragkun'} class="">
+				Chirag
+			</a>
+			. The source code is available on GitHub.
+		</p>
+	</footer>
+</Container>
+<Separator />
