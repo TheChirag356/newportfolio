cbebef4b98627fe07bdd3b5900f7cf5bf4d8bc09
Chirag Arora <54665586+TheChirag356@users.noreply.github.com>
2025-11-30 02:37:08 +0530

    chore: fix minor mistake in url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from "$app/environment";
 
 export const title = "Chirag's Blogs";
 export const description = "Thoughts, stories and ideas that would make the world a better place, maybe.";
-export const url = dev ? "http://localhost:5173/" : "https://chiragarora.xyz/";
+export const url = dev ? "http://localhost:5173" : "https://chiragarora.xyz";
