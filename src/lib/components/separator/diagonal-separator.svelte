02d48b961ab60b05c3151eaec8259da1910f47dd
TheChirag356 <arorachirag12005@gmail.com>
2025-11-30 17:29:18 +0530

    chore: css variables in place of harcoded value

--- src/lib/components/separator/diagonal-separator.svelte
+++ src/lib/components/separator/diagonal-separator.svelte
@@ -11,7 +11,7 @@
 
 <div
 	class={cn(
-		'pointer-events-none relative flex h-8 w-full items-center justify-center border-y bg-background text-white',
+		'pointer-events-none relative flex h-8 w-full items-center justify-center border-y bg-background text-foreground/60',
 		className
 	)}
 >
@@ -20,8 +20,8 @@
 		<div
 			class="pointer-events-none absolute inset-0 z-0 items-center justify-center"
 			style="background-image: repeating-linear-gradient(-60deg, 
-            rgba(255, 255, 255, 0.08) 0px, 
-            rgba(0, 255, 128, 0) 1px, 
+            var(--border) 0px, 
+            var(--border) 1px, 
             transparent 1px, 
             transparent 8px
             )"
